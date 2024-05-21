42906ae20d62bb02566f7bd6cd586f1978ee4970
Alanis Forero <alanis.forero@javeriana.edu.co>
2024-05-21 16:54:30 -0500

    Empece mi backend :)

---
+++ src/main/java/com/parcial/web/dto/CelularDTO.java
@@ -0,0 +1,25 @@
+package com.parcial.web.dto;
+
+import java.time.LocalDate;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.Accessors;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Accessors(chain = true)
+public class CelularDTO {
+    private Long id;
+    private String marca;
+    private String serial;
+    private LocalDate fechaDeCompra;
+    private int anioDeLanzamiento;
+    private double precio;
+    private String sistemaOperativo; 
+    private String gama; 
+}
